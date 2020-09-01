976e8860182a2381bc551f99b4011c2d81c4cd6d
Ryan <obviouslyghosts@gmail.com>
2020-09-01 13:47:41 -0400

    project setuo

---
+++ Console/Console/Source.cpp
@@ -0,0 +1,22 @@
+// my first program in C++
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int i;
+
+    string s;
+
+    cout << "please enter a string: ";
+
+    cin >> s;
+
+
+
+    cout << endl << "you entered : " << s;
+
+    return 0;
+}
